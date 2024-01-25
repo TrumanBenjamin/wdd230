b24020c342bc40fa00bf8782d32dfbac9e093dad
TrumanBenjamin <122401528+TrumanBenjamin@users.noreply.github.com>
2024-01-25 15:09:01 -0700

    PERFECTLY working website

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,8 +2,27 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 
 
 if (Date.parse(document.lastModified) != 0)
-    document.write('<p><hr><small><i>Last modified: '
-                   + document.lastModified
-                   + '</i></small>');
 
 document.getElementById("lastModified").innerHTML = document.lastModified; 
+
+// website visit counter
+
+var counterContainer = document.querySelector(".websiteCounter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem('page_view');
+
+if (visitCount) {
+    visitCount = Number(visitCount) + 1;
+    localStorage.setItem("page_view", visitCount);
+}
+    else {
+        visitCount = 1;
+        localStorage.setItem("page_view", 1);
+    }
+counterContainer.innerHTML = visitCount;
+
+resetButton.addEventListener("click", () => {
+    visitCount = 1;
+    localStorage.setItem("page_view", 1);
+    counterContainer.innerHTML = visitCount;
+});
