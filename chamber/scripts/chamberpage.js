e4ba26646599404617780e438efd693928b12e8b
TrumanBenjamin <122401528+TrumanBenjamin@users.noreply.github.com>
2024-03-11 18:27:47 -0600

    githubbbbbbbbb

--- chamber/scripts/chamberpage.js
+++ chamber/scripts/chamberpage.js
@@ -15,3 +15,25 @@ modeButton.addEventListener("click", () => {
 	body.classList.toggle('dark-mode');
 	main.classList.toggle('dark-mode');
 });
+
+// Banner
+const banner = document.getElementById('banner');
+
+const today = new Date();
+const dayOfWeek = today.getDay(); // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
+if (dayOfWeek >= 1 && dayOfWeek <= 3) {
+
+    setTimeout(function() {
+        banner.style.display = 'flex';
+        setTimeout(function() {
+            banner.classList.add('show');
+        }, 50); 
+    }, 2000);
+
+    function closeBanner() {
+        banner.classList.remove('show');
+        setTimeout(function() {
+            banner.style.display = 'none';
+        }, 1500); 
+    }
+}
--- scripts/coursehome.js
+++ scripts/coursehome.js
@@ -5,3 +5,37 @@ hambutton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hambutton.classList.toggle('open');
 });
+
+// Weather API
+function fetchWeatherData() {
+
+    const apiKey = '87301d110ab031ce1e2589211b8f3ddb';
+    const latitude = 43.8231;
+    const longitude = -111.7924; 
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const temperatureKelvin = data.main.temp;
+            const temperatureFahrenheit = (temperatureKelvin - 273.15) * 9/5 + 32;
+            const description = data.weather[0].description;
+            const iconCode = data.weather[0].icon;
+            const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
+
+            const weatherHtml = `
+                <p>Temperature: ${temperatureFahrenheit.toFixed(1)}°F</p>
+                <p>Description: ${description}</p>
+                <img src="${iconUrl}" alt="Weather Icon">
+            `;
+
+            document.getElementById('weatherInfo').innerHTML = weatherHtml;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            document.getElementById('weatherInfo').innerHTML = '<p>Error fetching weather data</p>';
+        });
+}
+window.onload = fetchWeatherData;
+
